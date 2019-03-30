05c92a16ec07a337bb468f4244e993cd6ec43399
tuannd <tuannd2207@gmail.com>
2019-03-30 22:05:50 +0700

    tuannd branch

--- english-courses/src/main/java/com/selflearning/englishcourses/EnglishCoursesApplication.java
+++ english-courses/src/main/java/com/selflearning/englishcourses/EnglishCoursesApplication.java
@@ -1,6 +1,5 @@
 package com.selflearning.englishcourses;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
